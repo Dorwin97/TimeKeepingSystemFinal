88fc8946a6fc5439d433ac3ff8e57e09b7fa0d4d
Dorwin97 <135909519+Dorwin97@users.noreply.github.com>
2023-07-05 19:08:54 -0400

    Add files via upload

--- src/Employee.java
+++ src/Employee.java
@@ -1,19 +1,28 @@
+// This code snippet defines a Java abstract class called `Employee`.
+
 public abstract class Employee {
-    private String name;
-    private int employeeId;
+    private String name; // Private member variable to store the employee's name.
+    private int employeeId; // Private member variable to store the employee's ID.
 
     public Employee(String name, int employeeId) {
-        this.name = name;
-        this.employeeId = employeeId;
+        this.name = name; // Initialize the employee's name.
+        this.employeeId = employeeId; // Initialize the employee's ID.
     }
 
-    public abstract double calculateSalary();
+    public abstract double calculateSalary(); // Abstract method for calculating the employee's salary.
 
     public String getName() {
-        return name;
+        return name; // Getter method to retrieve the employee's name.
     }
 
     public int getEmployeeId() {
-        return employeeId;
+        return employeeId; // Getter method to retrieve the employee's ID.
     }
 }
+
+// The `Employee` class is an abstract class that provides a blueprint for creating specific types of employees. It includes:
+// - Private member variables `name` and `employeeId` to store the employee's name and ID, respectively.
+// - A constructor that accepts the employee's name and ID and initializes the corresponding member variables.
+// - An abstract method `calculateSalary()` that must be implemented by concrete subclasses to calculate the employee's salary.
+// - Getter methods `getName()` and `getEmployeeId()` to retrieve the employee's name and ID, respectively.
+
