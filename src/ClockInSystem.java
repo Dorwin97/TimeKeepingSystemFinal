a9a1150b697635df5614ba8c16a6bf2f57d2e26a
Dorwin97 <135909519+Dorwin97@users.noreply.github.com>
2023-07-05 19:06:10 -0400

    Add files via upload

--- src/ClockInSystem.java
+++ src/ClockInSystem.java
@@ -1,4 +1,15 @@
+//Dorwin Dorwing1@ubmc.edu
+
+// This code snippet defines a Java interface called `ClockInSystem`.
+
 public interface ClockInSystem {
-    void clockIn();
-    void clockOut();
+    void clockIn(); // Method signature for clocking in an employee.
+    void clockOut(); // Method signature for clocking out an employee.
 }
+
+// The `ClockInSystem` interface represents a contract for implementing a clock-in system. It declares two methods:
+// 1. `clockIn()`: This method is responsible for recording an employee's clock-in time.
+// 2. `clockOut()`: This method is responsible for recording an employee's clock-out time.
+
+// Implementations of the `ClockInSystem` interface will need to provide the logic for these methods, such as capturing the current time, associating it with the employee, and storing the information in a suitable data structure or database.
+
